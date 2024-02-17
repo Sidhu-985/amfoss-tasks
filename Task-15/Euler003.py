23327e2f64ede34afca227e9f1e5abd9ee5feb08
Sidharth Sunil <143250587+Sidhu-985@users.noreply.github.com>
2024-02-17 23:51:14 +0530

    Update Euler003.py

--- Task-15/Euler003.py
+++ Task-15/Euler003.py
@@ -1,1 +1,13 @@
-
+t = int(input())
+for i in range(t):
+    N=int(input())
+    list1=[N]
+    list2=[]
+    for i in list1:
+        temp_list=[]
+        for j in range(2,i+1):
+            if i%j==0:
+                temp_list.append(j)
+                i=i//j
+        list2.append(max(temp_list))
+    print(max(temp_list))
