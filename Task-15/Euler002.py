925519871a072fa8642c00cb3ad4e5c07073f599
Sidharth Sunil <143250587+Sidhu-985@users.noreply.github.com>
2024-02-17 19:01:41 +0530

    Update Euler002.py

--- Task-15/Euler002.py
+++ Task-15/Euler002.py
@@ -1,1 +1,16 @@
-
+t=int(input())
+for _ in range(t):
+    N = int(input())
+    a,b=0,1
+    sum=0
+    lst=[]
+    
+    while N >= a:
+        lst.append(a)
+        a,b=b,a+b
+    
+    for i in lst:
+        if i%2==0:
+            sum+=i
+    
+    print(sum)
