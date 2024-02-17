9e51463a3b3c167b512470e06ba47d3467794cef
Sidharth Sunil <143250587+Sidhu-985@users.noreply.github.com>
2024-02-17 23:59:20 +0530

    Update Euler003.py

--- Task-15/Euler003.py
+++ Task-15/Euler003.py
@@ -1,13 +1,20 @@
-t = int(input())
-for i in range(t):
-    N=int(input())
-    list1=[N]
-    list2=[]
-    for i in list1:
-        temp_list=[]
-        for j in range(2,i+1):
-            if i%j==0:
-                temp_list.append(j)
-                i=i//j
-        list2.append(max(temp_list))
-    print(max(temp_list))
+n = int(input())
+for _ in range(n):
+    num = int(input())
+    
+    a = []  
+    b = []
+    for i in range(2, num+1):
+        is_prime = True
+        for j in range(2, int(i**0.5) + 1):
+            if i % j == 0:
+                is_prime = False
+                break
+        if is_prime:
+            a.append(i)
+    print(a)
+    for k in a :
+        if num % k == 0 :
+            b.append(k)
+    print(max(b))
+    
