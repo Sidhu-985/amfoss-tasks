754292279bffc8c008156c0f7a24001ed8436333
Sidharth Sunil <143250587+Sidhu-985@users.noreply.github.com>
2024-08-15 11:22:16 +0530

    Update Ques3.cpp

--- Task-03/Sub-Task 3/C++/Ques3.cpp
+++ Task-03/Sub-Task 3/C++/Ques3.cpp
@@ -4,12 +4,10 @@ using namespace std;
 int main() {
     int n = 5;
 
-    // Upper part
     for (int i = 1; i <= n; i += 2) {
         cout << string((n - i) / 2, ' ') << string(i, '*') << endl;
     }
 
-    // Lower part
     for (int i = n - 2; i > 0; i -= 2) {
         cout << string((n - i) / 2, ' ') << string(i, '*') << endl;
     }
