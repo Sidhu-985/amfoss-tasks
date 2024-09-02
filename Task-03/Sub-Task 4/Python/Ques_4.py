ff255f3f91fc65aa6f5d0398ca97516b218898d1
Sidharth Sunil <143250587+Sidhu-985@users.noreply.github.com>
2024-09-02 19:14:24 +0530

    Update Ques_4.py

--- Task-03/Sub-Task 4/Python/Ques_4.py
+++ Task-03/Sub-Task 4/Python/Ques_4.py
@@ -1,1 +1,21 @@
+with open("input.txt", "r") as file:
+    n = int(file.read().strip())
 
+with open("output.txt", "w") as file:
+    for i in range(1, n + 1, 2):
+        for j in range((n - i) // 2):
+            file.write(' ')
+        
+        for k in range(i):
+            file.write('*')
+        
+        file.write('\n')
+
+    for i in range(n - 2, 0, -2):
+        for j in range((n - i) // 2):
+            file.write(' ')
+        
+        for k in range(i):
+            file.write('*')
+        
+        file.write('\n')
