079589c76267a3179ce9cd231814988917079c64
Sidharth Sunil <143250587+Sidhu-985@users.noreply.github.com>
2024-09-02 19:26:57 +0530

    Update Ques_4.rb

--- Task-03/Sub-Task 4/Ruby/Ques_4.rb
+++ Task-03/Sub-Task 4/Ruby/Ques_4.rb
@@ -1,1 +1,27 @@
+def generate_diamond(n)
+  diamond = []
 
+  (1..n).step(2) do |i|
+    spaces = ' ' * ((n - i) / 2)
+    stars = '*' * i
+    diamond << spaces + stars
+  end
+
+  (n - 2).step(1, -2) do |i|
+    spaces = ' ' * ((n - i) / 2)
+    stars = '*' * i
+    diamond << spaces + stars
+  end
+
+  diamond
+end
+
+n = File.read('input.txt').to_i
+
+diamond_pattern = generate_diamond(n)
+
+File.open('output.txt', 'w') do |file|
+  file.puts diamond_pattern
+end
+
+puts "Diamond pattern written to output.txt"
