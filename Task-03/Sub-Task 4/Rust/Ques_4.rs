d0e688b0310c61e035e0396f8dae07520c041fae
Sidharth Sunil <143250587+Sidhu-985@users.noreply.github.com>
2024-09-02 19:28:00 +0530

    Update Ques_4.rs

--- Task-03/Sub-Task 4/Rust/Ques_4.rs
+++ Task-03/Sub-Task 4/Rust/Ques_4.rs
@@ -1,1 +1,34 @@
+use std::fs;
 
+fn generate_diamond(n: usize) -> Vec<String> {
+    let mut diamond = Vec::new();
+
+    for i in (1..=n).step_by(2) {
+        let spaces = " ".repeat((n - i) / 2);
+        let stars = "*".repeat(i);
+        diamond.push(format!("{}{}", spaces, stars));
+    }
+
+    for i in (1..n).rev().step_by(2) {
+        let spaces = " ".repeat((n - i) / 2);
+        let stars = "*".repeat(i);
+        diamond.push(format!("{}{}", spaces, stars));
+    }
+
+    diamond
+}
+
+fn main() {
+    let n: usize = fs::read_to_string("input.txt")
+        .expect("Unable to read input.txt")
+        .trim()
+        .parse()
+        .expect("Input is not a valid number");
+
+    let diamond_pattern = generate_diamond(n);
+
+    fs::write("output.txt", diamond_pattern.join("\n"))
+        .expect("Unable to write to output.txt");
+
+    println!("Diamond pattern written to output.txt");
+}
