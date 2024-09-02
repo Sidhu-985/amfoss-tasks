c791c9258f1d506b584ddca463d9ed75f9950a8c
Sidharth Sunil <143250587+Sidhu-985@users.noreply.github.com>
2024-09-02 19:26:02 +0530

    Update Ques_4.hs

--- Task-03/Sub-Task 4/Haskell/Ques_4.hs
+++ Task-03/Sub-Task 4/Haskell/Ques_4.hs
@@ -1,1 +1,13 @@
+import Data.List (intercalate)
 
+generateDiamond :: Int -> [String]
+generateDiamond n = topHalf ++ bottomHalf
+  where
+    topHalf = [replicate (n - i) ' ' ++ replicate (2 * i - 1) '*' | i <- [1..n]]
+    bottomHalf = reverse $ drop 1 topHalf
+
+main :: IO ()
+main = do
+  n <- read <$> readFile "input.txt"
+  writeFile "output.txt" $ unlines $ generateDiamond n
+  putStrLn "Diamond pattern written to output.txt"
